3e8d6607bea54071a16c71e18b9772f39be39f29
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-04-10 23:07:35 +0200

    .

--- src/faunaDB.js
+++ src/faunaDB.js
@@ -1,6 +1,5 @@
 import faunadb from "faunadb"
 let client = new faunadb.Client({ secret: process.env.REACT_APP__SECRET });
 let q = faunadb.query;
-console.log(process.env.REACT_APP__SECRET)
 export {client,q}
 
